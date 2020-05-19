710823235ba854295ed4a6ba322fc6c25684305a
suteki90 <64235925+suteki90@users.noreply.github.com>
2020-05-19 12:22:23 -0400

    Create package.py

---
+++ package.py
@@ -0,0 +1,18 @@
+{
+    "name": "Bop",
+    "version": "0",
+    "description": "A fun bot made by s u t e k i#3477",
+    "main": "bot.py",
+    "dependencies": {
+    "discord.py",
+    }
+
+}
+"devDependencies": {},
+"scripts": {
+
+},
+"author": ""
+"liscense": "ISC"
+
+}
